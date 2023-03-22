575fd550459e6ded98fa0a49cb3bad7291effdac
Danish <danish_s@outlook.com>
2023-03-23 02:12:52 +0500

    Update app.js
    - JS: Convert to lower case

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -7,3 +7,13 @@ txt.toUpperCase(txt);
 
 // Output
 console.log(txt);
+
+// JS: Convert to lower case
+
+var txt = "Interns Pakistan";
+
+// Write your code here
+txt.toLowerCase(txt);
+
+// Output
+console.log(txt);
