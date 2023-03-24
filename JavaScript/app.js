6b30dcde9b72f43e1c29ef0be96ad834feb06abd
Danish <danish_s@outlook.com>
2023-03-24 23:52:51 +0500

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,17 @@
-// ES6: Using let keyword within a function
+// ES6: Declare a Read-Only Variable with the const Keyword
 
-function sumFunction(value1, value2) {
-  let result = 0;
-  result = value1 + value2;
-  return result;
+function printManyTimes(str) {
+  //  The purpose of use strict is to indicate that the code should be executed in strict mode, With strict mode, you can not, for example, use undeclared variables.
+  "use strict";
+
+  // Only change code below this line
+
+  let sentence = str;
+  for (let i = 0; i < str.length; i += 2) {
+    console.log(sentence);
+  }
+
+  // Only change code above this line
 }
+
+printManyTimes("InternsProgram");
