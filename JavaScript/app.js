bfddb9c5fa15430d1905a5f23a4b826013bac845
Danish <danish_s@outlook.com>
2023-03-24 23:43:49 +0500

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,53 +1,12 @@
-// JS: Convert to upper case
+let a = 50; // declared First
+let b = 100;
 
-var txt = "Interns Pakistan";
-
-// Write your code here
-txt.toUpperCase(txt);
-
-// Output
-console.log(txt);
-
-// JS: Convert to lower case
-
-var txt = "Interns Pakistan";
-
-// Write your code here
-txt.toLowerCase(txt);
-
-// Output
-console.log(txt);
-
-// JS: Get value from an array item
-
-var cars = ["Saab", "Volvo", "BMW"];
-
-// Write your code here
-var x;
-x = cars[1];
-
-// JS: Change value of an array item
-
-var cars = ["Volvo", "Jeep", "Mercedes"];
-
-// Write your code here
-
-cars[0] = "Ford";
-
-// JS: Get length of an array
-
-var cars = ["Volvo", "Jeep", "Mercedes"];
-
-// Write your code here
-
-alert(cars.length);
-
-// JS: Loop to generate numbers
-
-var i;
-
-// For loop
-
-for (var i = 0; i < 10; i++) {
-  console.log(i);
+if (true) {
+  let a = 60; // declared Again
+  var c = 10;
+  console.log(a / c); // 6
+  console.log(b / c); // 10
 }
+
+console.log(c); // 10
+console.log(a); // 50
