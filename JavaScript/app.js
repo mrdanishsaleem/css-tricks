d00223f97b569efa15dd58c6d400e0c195bd25b6
Danish <danish_s@outlook.com>
2023-03-26 16:17:42 +0500

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,8 @@
-// ES6: Write Arrow Functions with Parameters
+// ES6: Write Arrow Functions with Parameters without the argument parentheses
 
-let subNumbers = (value1, value2) => {
-  console.log(value2 - value1);
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
 };
+
+console.log(myConcat([1, 2], [3, 4, 5]));
