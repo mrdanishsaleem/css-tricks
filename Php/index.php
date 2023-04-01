6ac27f2846b3e3363e5ff67c5047df0e49574601
Danish <danish_s@outlook.com>
2023-04-01 15:16:14 +0500

    Challenge # 3 PHP: Declaring Variables

--- Php/index.php
+++ Php/index.php
@@ -12,3 +12,18 @@ echo "This is PHP";
 // echo "HelloWorld to PHP";
 
 ?>
+
+<!-- Challenge # 3 PHP: Declaring Variables -->
+
+<?php
+
+// Example
+$txt = 'Hello world!';
+$x = 5;
+$y = 10.5;
+
+// Don't write above this line
+
+$interns = 'Pakistan';
+
+?>
